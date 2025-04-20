dd5081111c5aa4234237e754ca3c30364dc5dd9f
cosmic-vista <akkashyadav61@gmail.com>
2025-04-20 23:04:34 +0530

    added get all post controller to retrieve all posts from the database

--- backend/controller/getPostController.js
+++ backend/controller/getPostController.js
@@ -5,8 +5,23 @@ import Post from "../database/model/postSchema.js";
 export const getPost = async (req, res) => {
   try {
     const post = await Post.find({});
+    // getting total post
+    const totalPost = await Post.countDocuments();
 
-    res.status(200).json(post, "All posts are fetched ");
+    // setting date for finding the post crated in lastMonth
+
+    const current = new Date();
+
+    const oneMonthago = new Date(
+      current.getFullYear(),
+      current.getMonth() - 1,
+      current.getDate()
+    );
+
+    const prevMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthago },
+    });
+    res.status(200).json({ post, totalPost, prevMonthPosts });
   } catch (error) {
     console.error("Error fetching posts:", error);
     res.status(500).json({ message: "Failed to fetch posts" });
