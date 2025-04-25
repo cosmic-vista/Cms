cb9334c40fcadd0d5151f03ab26145f93a21f8e3
cosmic-vista <akkashyadav61@gmail.com>
2025-04-25 23:56:50 +0530

    defined update post controller

---
+++ backend/controller/updatePost.js
@@ -0,0 +1,26 @@
+import Post from "../database/model/postSchema.js";
+
+export const updatePost = async (req, res) => {
+  const postId = req.params.id;
+  const { title, description, category, image } = req.body;
+
+  try {
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    // setting all the values that are send by frontend
+    post.title = title || post.title;
+    post.description = description || post.description;
+    post.category = category || post.category;
+    post.image = image || post.image;
+
+    const updatedPost = await post.save();
+
+    return res.status(200).json(updatedPost);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
