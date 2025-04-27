e681c2bb49b8cf215b849f7c417b6401a62d043c
cosmic-vista <akkashyadav61@gmail.com>
2025-04-28 00:19:15 +0530

    added the createcomment page with props passing

--- frontend/src/components/SharedHeader/CommentLayout.jsx
+++ frontend/src/components/SharedHeader/CommentLayout.jsx
@@ -4,26 +4,52 @@ import { useSelector } from "react-redux";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "../ui/button";
 import { useState } from "react";
+import axios from "axios";
 
-const CommentLayout = () => {
+const CommentLayout = ({ postId, userId }) => {
   const { currentUser } = useSelector((state) => state.user);
   const [comment, setComment] = useState();
-  const [remaining, setRemaining] = useState(200);
+  const [remaining, setRemaining] = useState(500);
 
-  useEffect(() => {
-    console.log(comment);
-  }, [comment]);
+  const username = currentUser.user.username;
+  const picture = currentUser.user.profilePic;
+  const email = currentUser.user.email;
 
+  const handelClick = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/user/comment",
+        {
+          username,
+          email,
+          postId,
+          picture,
+          comment,
+          userId,
+        },
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.data.success) {
+        setComment("");
+        setRemaining(500);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div className=" max-w-3xl mx-auto w-full p-3">
+    <div className=" max-w-3xl mx-auto w-full p-3 mt-6">
       {currentUser ? (
-        <div className="flex items-center gap-1 text-gary-500 text-sm">
+        <div className="flex items-center gap-1 text-gray-500 text-sm">
           <p>Signed as :</p>
-          <img
-            src={currentUser.user.profilePic}
-            className="h-5 w-5 object-fill rounded-full"
-          />
-          @{currentUser.user.username}
+          <img src={picture} className="h-5 w-5 object-fill rounded-full" />@
+          {username}
         </div>
       ) : (
         <>
@@ -35,16 +61,17 @@ const CommentLayout = () => {
       {currentUser && (
         <form className="border-2 border-blue-200 rounded-md p-4">
           <Textarea
+            value={comment}
             className="border border-gray-500 focus-visible:ring-0 focus-visible:ring-offset-0"
             placeholder="Add your comment...."
             onChange={(e) => {
               setComment(e.target.value);
-              setRemaining(200 - e.target.value.length);
+              setRemaining(500 - e.target.value.length);
             }}
           />
           <div className="flex justify-between items-center mt-5 ">
             <p className="text-gray-400">length remaining {remaining} </p>
-            <Button>post</Button>
+            <Button onClick={handelClick}>post</Button>
           </div>
         </form>
       )}
