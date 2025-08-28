e20162c030e6c904de272c5ca1a78cb6f710cb95
Akash yadav <akkashyadav61@gmail.com>
2025-08-28 16:22:37 +0530

    Update userRoute.js
    added  router for the comment

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -7,7 +7,7 @@ import { deleteAccount, deleteUser } from "../controller/DeleteAccount.js";
 import { updateProfile } from "../controller/UpdateUser.js";
 import { getAllUser } from "../controller/getAlluser.js";
 import { createcomment } from "../controller/createComment.js";
-import { getPostComment, getAllComment } from "../controller/getPostComment.js";
+import { getPostComment, getAllComment ,deleteComment} from "../controller/getPostComment.js";
 
 router.put("/changepassword", verifyToken, changePassword);
 router.put("/update", verifyToken, updateProfile);
@@ -16,5 +16,6 @@ router.get("/get-all", verifyToken, getAllUser);
 router.post("/comment", createcomment);
 router.get("/getPostComment/:postId", getPostComment);
 router.get("/get-all-Comment/", verifyToken, getAllComment);
+router.delete("/delete-comment/:commentId", verifyToken, deleteComment);
 
 export default router;
