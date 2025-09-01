d5617d0d61b2631d6777010aa7db2a52d8fa64e8
Akash yadav <akkashyadav61@gmail.com>
2025-09-01 21:36:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ connectDB();
 
 const app = express();
 
-const frontendOrigin = "http://localhost:5173";
+const frontendOrigin = "https://project1-ccdb4.web.app/";
 
 // Middleware for parsing JSON and URL-encoded bodies with increased limit
 app.use(express.json({ limit: "20mb" }));
